503b2147cbc80fe3146974f882a58f14a5c14048
VidWantsToDevelop <standfornew@gmail.com>
2022-06-19 18:20:34 -0400

    second push

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,60 @@ const Header = () => {
   const [navIsShown, setIsShown] = useState(false)
   const [logoIsShown, setLogoIsShown] = useState(false)
 
+  useEffect(() => {
+    var TxtRotate = function (el, toRotate, period) {
+      this.toRotate = toRotate
+      this.el = el
+      this.loopNum = 0
+      this.period = parseInt(period, 10) || 2000
+      this.txt = ''
+      this.tick()
+      this.isDeleting = false
+    }
+
+    TxtRotate.prototype.tick = function () {
+      var i = this.loopNum % this.toRotate.length
+      var fullTxt = this.toRotate[i]
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1)
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1)
+      }
+
+      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
+
+      var that = this
+      var delta = 300 - Math.random() * 100
+
+      if (this.isDeleting) {
+        delta /= 2
+      }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period
+        this.isDeleting = true
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false
+        this.loopNum++
+        delta = 500
+      }
+
+      setTimeout(function () {
+        that.tick()
+      }, delta)
+    }
+
+    var elements = document.getElementsByClassName('txt-rotate')
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate')
+      var period = elements[i].getAttribute('data-period')
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period)
+      }
+    }
+  }, [])
+
   return (
     <>
       <header>
@@ -38,12 +92,14 @@ const Header = () => {
             </div>
           </div>
           <div className={`subheading ${logoIsShown ? 'shown' : 'hidden'}`}>
-            <div className='container py-5'>
-              <div className='output' id='output'>
-                <h1 className='cursor'></h1>
-                <p></p>
-              </div>
-            </div>
+            <h1>
+              This pen is
+              <span
+                className='txt-rotate'
+                data-period='2000'
+                data-rotate='[ "nerdy.", "simple.", "pure JS.", "pretty.", "fun!" ]'
+              ></span>
+            </h1>
           </div>
         </div>
         <nav className='header-rightSide'>
