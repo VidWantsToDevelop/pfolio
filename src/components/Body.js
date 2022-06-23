2d6f64765a4a379ced9c7cb2519fc5e519fb9158
VidWantsToDevelop <standfornew@gmail.com>
2022-06-22 22:47:42 -0400

    Header hide/reveal function has been set

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GiSharpSmile } from 'react-icons/gi'
 
 const Body = () => {
+  const text = [
+    'Good morning/evening',
+    'Just trying my best',
+    'A curious programmer',
+    'Thanks for visiting',
+  ]
+  useEffect(() => {
+    const app = document.querySelector('.App')
+    let lastScrollPos = app.scrollTop
+
+    const header = document.querySelector('header')
+
+    app.onscroll = () => {
+      let currentScrollPost = app.scrollTop
+      if (currentScrollPost > lastScrollPos) {
+        header.style.top = '-8rem'
+      } else {
+        header.style.top = '1rem'
+      }
+      lastScrollPos = currentScrollPost
+    }
+  }, [])
+
   return (
     <>
       <main>
         <div className='hero'>
           <div>
-            <GiSharpSmile size={150} color={'#ffffff'} />
+            <div className='header-leftSide'>
+              <div className='logo'>
+                <div className='letter shown'>DAVID</div>
+              </div>
+              <div className={`subheading shown`}>
+                <h1
+                  className='typewriter-text'
+                  onAnimationIteration={(e) => {
+                    if (e.animationName === 'typewriter') {
+                      console.log(e)
+                      console.log(Math.random() * 3)
+                      e.target.innerText = text[Math.floor(Math.random() * 3)]
+                    }
+                  }}
+                >
+                  Good morning/evening
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
         <div className='about'>
           <h2>Briefly about me 👨‍💻</h2>
           <h3>
-            #Time is money, and I don't want to waste yours. So let's start
-            right away
+            #Time is money, and I don't want to waste your. So let's start right
+            away
           </h3>
           <p>
-            My name is David. I am a student (at the moment of creation of this
+            My name is David. I am a student (at the moment of creating this
             website) at the Georgian College, my major is - Computer
-            Programming. I'm a type of a programmer who is always learning
-            something new, even in my leisure I spend the time creating
+            Programming. I'm a type of programmer who is always learning
+            something new, even in my leisure, I spend the time creating
             different projects using different programming languages. In my
             career I was working with a whole list of technologies and
             platforms: OO programming languages -{'  '}
@@ -31,15 +72,16 @@ const Body = () => {
             </span>{' '}
             (P.S this website was built using React JS and React Icons only)
             <span>, JQuery, ThreeJS, and Django 🐍</span> (have some projects on
-            my gitHub account)&& I have also challenged myself in learning more
+            my GitHub account)&& I have also challenged myself in learning more
             about mobile development -{' '}
             <span>React Native, Android Studio (Java, not Kotlin)</span> &&
             Recently, put some effort to learn more about Cloud systems ☁️,
             particularly{' '}
             <span>AWS (EC2, Lambda functions, S3 bucket, API Gateway) </span>and
-            <span> Google Cloud Platform </span>. && Most of the spring 2022
-            spent on learning <span>Python scripting (Windows and Linux)</span>.
-            And many more, but as I said time is money
+            <span> Google Cloud Platform </span>. && I have spent most of the
+            spring 2022 on learning{' '}
+            <span>Python scripting (Windows and Linux)</span>. And many more,
+            but as I said time is money
           </p>
         </div>
         <div className='projects'>
@@ -71,15 +113,15 @@ const Body = () => {
               <p>
                 Yeah, I know how it sounds, but here it is - Duck market. As I
                 have already mentioned before, right now I'm learning more about
-                new JS framework called ThreeJS and this project is the way I
-                learn more about implementation of the 3D into the web. The
-                website is still in the stage of the production (I'm sorry but
-                you can't really buy a duck), but it has almost all the
-                front-end features. The website is based on the ReactJS (states
-                are managed by Context API and useReducer hook), ReactJS
-                Bootstrap, and as I said ThreeJS. <br /> I also have a few more
-                projects, but they are all collecting dust on the shelevs of the
-                github
+                a new JS framework called ThreeJS and this project is the way I
+                learn more about the implementation of the 3D into the web. The
+                website is still in the stage of production (I'm sorry but you
+                can't really buy a duck), but it has almost all the front-end
+                features. The website is based on the ReactJS (states are
+                managed by Context API and useReducer hook), ReactJS Bootstrap,
+                and as I said ThreeJS.
+                <br /> I also have a few more projects, but they are all
+                collecting dust on the shelves of the GitHub
               </p>
             </div>
           </div>
@@ -88,11 +130,11 @@ const Body = () => {
           <h2>Conclusion 🏁</h2>
           <h3>Finally, we are here. </h3>
           <p>
-            Congratulations, you made all the way through and here is a little
-            conclusion. I know this website may look a bit like it was made only
-            in a few days (in fact it was), but I need to focus more on classes
-            in the college, this is why I don't have much time, but I promise I
-            wll make it better 🙏.
+            Congratulations, you made it all the way through and here is a
+            little conclusion. I know this website may look a bit like it was
+            made only in a few days (in fact it was), but I need to focus more
+            on classes at the college, this is why I don't have much time, but I
+            promise I will make it better 🙏.
             <br />
             <span>
               #Thank you for the time you have spent, you can also check some
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,107 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { SiGithub } from 'react-icons/si'
 import { BsLinkedin } from 'react-icons/bs'
-import { GiDuck } from 'react-icons/gi'
+import { GiDuck, GiSharpSmile } from 'react-icons/gi'
 
 const Header = () => {
   const [navIsShown, setIsShown] = useState(false)
   const [logoIsShown, setLogoIsShown] = useState(false)
 
-  useEffect(() => {
-    var TxtRotate = function (el, toRotate, period) {
-      this.toRotate = toRotate
-      this.el = el
-      this.loopNum = 0
-      this.period = parseInt(period, 10) || 2000
-      this.txt = ''
-      this.tick()
-      this.isDeleting = false
-    }
-
-    TxtRotate.prototype.tick = function () {
-      var i = this.loopNum % this.toRotate.length
-      var fullTxt = this.toRotate[i]
-
-      if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1)
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1)
-      }
-
-      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
-
-      var that = this
-      var delta = 300 - Math.random() * 100
-
-      if (this.isDeleting) {
-        delta /= 2
-      }
-
-      if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period
-        this.isDeleting = true
-      } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false
-        this.loopNum++
-        delta = 500
-      }
-
-      setTimeout(function () {
-        that.tick()
-      }, delta)
-    }
-
-    var elements = document.getElementsByClassName('txt-rotate')
-    for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate')
-      var period = elements[i].getAttribute('data-period')
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period)
-      }
-    }
-  }, [])
-
   return (
     <>
       <header>
-        <div className='header-leftSide'>
-          <div className='logo'>
-            <div
-              className={`letter initial ${logoIsShown ? 'shown' : 'hidden'}`}
-              onClick={() => {
-                setLogoIsShown(!logoIsShown)
-              }}
-            >
-              D
-            </div>
-            <div
-              className={`letter second ${logoIsShown ? 'shown' : 'hidden'}`}
-            >
-              A
-            </div>
-            <div className={`letter third ${logoIsShown ? 'shown' : 'hidden'}`}>
-              V
-            </div>
-            <div
-              className={`letter fourth ${logoIsShown ? 'shown' : 'hidden'}`}
-            >
-              I
-            </div>
-            <div className={`letter fifth ${logoIsShown ? 'shown' : 'hidden'}`}>
-              D
-            </div>
-          </div>
-          <div className={`subheading ${logoIsShown ? 'shown' : 'hidden'}`}>
-            <h1>
-              This pen is
-              <span
-                className='txt-rotate'
-                data-period='2000'
-                data-rotate='[ "nerdy.", "simple.", "pure JS.", "pretty.", "fun!" ]'
-              ></span>
-            </h1>
-          </div>
-        </div>
+        <GiSharpSmile color={'#3f3'} size={35} />
         <nav className='header-rightSide'>
           <div
             className={`header-background ${navIsShown ? 'shown' : 'hidden'}`}
